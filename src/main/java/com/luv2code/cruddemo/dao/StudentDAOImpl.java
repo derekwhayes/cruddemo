333ade4d5928be493b6ee841d6d690e3a621a063
Derek Hayes <derek2099@gmail.com>
2024-04-20 17:57:50 -0500

    add update method

--- src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -57,5 +57,11 @@ public class StudentDAOImpl implements StudentDAO{
         return theQuery.getResultList();
     }
 
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
 
 }
